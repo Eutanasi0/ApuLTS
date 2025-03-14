18ed9acf4483e962b31861b18f0b9092c5e7a783
Eutanasi0 <benjaminflores4e@gmail.com>
2025-03-13 23:57:55 -0500

    add about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,16 @@
+import donk from '../assets/donk.gif';
+import './about.css';
 const About = () => {
   return <div className="allpagec">
-    
+    <p>
+      Hello, my name is Benjamin(ApuLTS) im studyng Software engineering (4y).<br />
+      <br />
+      I'm a kind of web developer, btw I'm more interested in front-end development, <br />
+      I've always been drawn to minimalist stuff and things like this personal <br />
+      website. <br />
+      I hope Courier was the right choice for the font-family of this site. <br />
+    </p>
+    <img className="donk" src={donk} alt="donk" />
   </div>
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,11 @@ import "./home.css";
 import Buy from "../assets/vector.gif";
 const Home = () => {
   return <div className="allpagec">
-      <p>Hi, im a Swe student</p>
-      <p>Im kind of web developer</p>
-      <img className="buy" src={Buy} alt="buy" />
+    <p>Hi, im a Swe student <br />
+       Im kind of web developer   
+    </p>
+      
+    <img className="buy" src={Buy} alt="buy" />
 
     
     
