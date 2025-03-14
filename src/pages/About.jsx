0450be9e88d5ba5ff19f50f1c9e0b790607a08d8
Eutanasi0 <benjaminflores4e@gmail.com>
2025-03-14 00:25:02 -0500

    add contactme

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
       I'm a kind of web developer, btw I'm more interested in front-end development, <br />
       I've always been drawn to minimalist stuff and things like this personal <br />
       website. <br />
-      I hope Courier was the right choice for the font-family of this site. <br />
+      I hope Courier was the right choice for the font-family on this site. <br />
     </p>
     <img className="donk" src={donk} alt="donk" />
   </div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,16 @@
+import './contact.css';
 const Contact = () => {
+
+  const to = (url) => {
+    window.open(url, "_blank");
+  };
+
   return <div className="allpagec">
-    
+    <p>
+      this is my <button className="githubbutton" onClick={() => to("https://github.com/Eutanasi0")} >github</button> account <br />
+      I am in <button className="xbutton" onClick={() => to("https://x.com/apuleftthecrew")}>X</button> too <br />
+      You can find me on <button className="linkedinbutton" onClick={() => to("www.linkedin.com/in/benjaminfloresgomez")}>linkedin</button> <br />
+    </p>
   </div>
 };
 
